f741b290dbea0e30b65527f8d2be7431009b2542
Simon Pichugin <simon.pichugin@gmail.com>
2020-08-24 22:29:03 +0200

    Fetch all of the issues and PRs

--- lib/patogith/__init__.py
+++ lib/patogith/__init__.py
@@ -6,6 +6,32 @@
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 
+import json
+import urllib.request
+from os import listdir
+from os.path import isfile, join
 
-def run(pagure_creds={}, github_creds={}, bugzilla_creds={}):
-    pass
+ISSUES_DIR = "tickets"
+PRS_DIR = "requests"
+REPO_NAME = "389-ds-base"
+
+
+def run(args):
+    issues = [f for f in listdir(ISSUES_DIR) if isfile(join(ISSUES_DIR, f))]
+    prs = [f for f in listdir(PRS_DIR) if isfile(join(PRS_DIR, f))]
+
+    issue_jsons = []
+    for issue in issues:
+        with open(join(ISSUES_DIR, issue)) as f:
+            issue_jsons.append(json.loads(f.read()))
+
+    pr_jsons = []
+    for pr in prs:
+        with open(join(PRS_DIR, pr)) as f:
+            pr_jsons.append(json.loads(f.read()))
+
+    print(pr_jsons[0]['id'])
+    print(issue_jsons[0]['id'])
+    with urllib.request.urlopen(f'https://www.pagure.io/{REPO_NAME}/pull-request/{pr_jsons[0]["id"]}.patch') as f:
+        pr_patch = f.read().decode('utf-8')
+    print(pr_patch)
